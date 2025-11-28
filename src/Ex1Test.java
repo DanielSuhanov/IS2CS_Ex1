31c0f3636d6b7a8b2dc6456477d1e2909842e507
dansu <dansuh5@gmail.com>
2025-11-28 22:42:54 +0200

    Added basic test for sameValue

--- src/Ex1Test.java
+++ src/Ex1Test.java
@@ -14,12 +14,13 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 
 class Ex1Test {
-	static final double[] P1 ={2,0,3, -1,0}, P2 = {0.1,0,1, 0.1,3};
-	static double[] po1 = {2,2}, po2 = {-3, 0.61, 0.2};;
-	static double[] po3 = {2,1,-0.7, -0.02,0.02};
+	static final double[] P1 = {2, 0, 3, -1, 0}, P2 = {0.1, 0, 1, 0.1, 3};
+	static double[] po1 = {2, 2}, po2 = {-3, 0.61, 0.2};
+	;
+	static double[] po3 = {2, 1, -0.7, -0.02, 0.02};
 	static double[] po4 = {-3, 0.61, 0.2};
-	
- 	@Test
+
+	@Test
 	/**
 	 * Tests that f(x) == poly(x).
 	 */
@@ -31,6 +32,7 @@ class Ex1Test {
 		assertEquals(fx1, 4, Ex1.EPS);
 		assertEquals(fx2, 6, Ex1.EPS);
 	}
+
 	@Test
 	/**
 	 * Tests that p1(x) + p2(x) == (p1+p2)(x)
@@ -43,6 +45,7 @@ class Ex1Test {
 		double f12x = Ex1.f(po12, x);
 		assertEquals(f1x + f2x, f12x, Ex1.EPS);
 	}
+
 	@Test
 	/**
 	 * Tests that p1+p2+ (-1*p2) == p1
@@ -54,6 +57,7 @@ class Ex1Test {
 		double[] p1 = Ex1.add(p12, pp2);
 		assertTrue(Ex1.equals(p1, po1));
 	}
+
 	@Test
 	/**
 	 * Tests that p1+p2 == p2+p1
@@ -63,6 +67,7 @@ class Ex1Test {
 		double[] p21 = Ex1.add(po2, po1);
 		assertTrue(Ex1.equals(p12, p21));
 	}
+
 	@Test
 	/**
 	 * Tests that p1+0 == p1
@@ -71,6 +76,7 @@ class Ex1Test {
 		double[] p1 = Ex1.add(po1, Ex1.ZERO);
 		assertTrue(Ex1.equals(p1, po1));
 	}
+
 	@Test
 	/**
 	 * Tests that p1*0 == 0
@@ -79,6 +85,7 @@ class Ex1Test {
 		double[] p1 = Ex1.mul(po1, Ex1.ZERO);
 		assertTrue(Ex1.equals(p1, Ex1.ZERO));
 	}
+
 	@Test
 	/**
 	 * Tests that p1*p2 == p2*p1
@@ -88,28 +95,30 @@ class Ex1Test {
 		double[] p21 = Ex1.mul(po2, po1);
 		assertTrue(Ex1.equals(p12, p21));
 	}
+
 	@Test
 	/**
 	 * Tests that p1(x) * p2(x) = (p1*p2)(x),
 	 */
 	void testMulDoubleArrayDoubleArray() {
-		double[] xx = {0,1,2,3,4.1,-15.2222};
+		double[] xx = {0, 1, 2, 3, 4.1, -15.2222};
 		double[] p12 = Ex1.mul(po1, po2);
-		for(int i = 0;i<xx.length;i=i+1) {
+		for (int i = 0; i < xx.length; i = i + 1) {
 			double x = xx[i];
 			double f1x = Ex1.f(po1, x);
 			double f2x = Ex1.f(po2, x);
 			double f12x = Ex1.f(p12, x);
-			assertEquals(f12x, f1x*f2x, Ex1.EPS);
+			assertEquals(f12x, f1x * f2x, Ex1.EPS);
 		}
 	}
+
 	@Test
 	/**
 	 * Tests a simple derivative examples - till ZERO.
 	 */
 	void testDerivativeArrayDoubleArray() {
-		double[] p = {1,2,3}; // 3X^2+2x+1
-		double[] pt = {2,6}; // 6x+2
+		double[] p = {1, 2, 3}; // 3X^2+2x+1
+		double[] pt = {2, 6}; // 6x+2
 		double[] dp1 = Ex1.derivative(p); // 2x + 6
 		double[] dp2 = Ex1.derivative(dp1); // 2
 		double[] dp3 = Ex1.derivative(dp2); // 0
@@ -118,34 +127,40 @@ class Ex1Test {
 		assertTrue(Ex1.equals(Ex1.ZERO, dp3));
 		assertTrue(Ex1.equals(dp4, dp3));
 	}
+
 	@Test
-	/** 
+	/**
 	 * Tests the parsing of a polynom in a String like form.
 	 */
 	public void testFromString() {
-		double[] p = {-1.1,2.3,3.1}; // 3.1X^2+ 2.3x -1.1
+		double[] p = {-1.1, 2.3, 3.1}; // 3.1X^2+ 2.3x -1.1
 		String sp2 = "3.1x^2 +2.3x -1.1";
 		String sp = Ex1.poly(p);
 		double[] p1 = Ex1.getPolynomFromString(sp);
 		double[] p2 = Ex1.getPolynomFromString(sp2);
 		boolean isSame1 = Ex1.equals(p1, p);
 		boolean isSame2 = Ex1.equals(p2, p);
-		if(!isSame1) {fail();}
-		if(!isSame2) {fail();}
+		if (!isSame1) {
+			fail();
+		}
+		if (!isSame2) {
+			fail();
+		}
 		assertEquals(sp, Ex1.poly(p1));
 	}
+
 	@Test
 	/**
 	 * Tests the equality of pairs of arrays.
 	 */
 	public void testEquals() {
-		double[][] d1 = {{0}, {1}, {1,2,0,0}};
-		double[][] d2 = {Ex1.ZERO, {1+ Ex1.EPS/2}, {1,2}};
-		double[][] xx = {{-2* Ex1.EPS}, {1+ Ex1.EPS*1.2}, {1,2, Ex1.EPS/2}};
-		for(int i=0;i<d1.length;i=i+1) {
+		double[][] d1 = {{0}, {1}, {1, 2, 0, 0}};
+		double[][] d2 = {Ex1.ZERO, {1 + Ex1.EPS / 2}, {1, 2}};
+		double[][] xx = {{-2 * Ex1.EPS}, {1 + Ex1.EPS * 1.2}, {1, 2, Ex1.EPS / 2}};
+		for (int i = 0; i < d1.length; i = i + 1) {
 			assertTrue(Ex1.equals(d1[i], d2[i]));
 		}
-		for(int i=0;i<d1.length;i=i+1) {
+		for (int i = 0; i < d1.length; i = i + 1) {
 			assertFalse(Ex1.equals(d1[i], xx[i]));
 		}
 	}
@@ -155,40 +170,43 @@ class Ex1Test {
 	 * Tests is the sameValue function is symmetric.
 	 */
 	public void testSameValue2() {
-		double x1=-4, x2=0;
-		double rs1 = Ex1.sameValue(po1,po2, x1, x2, Ex1.EPS);
-		double rs2 = Ex1.sameValue(po2,po1, x1, x2, Ex1.EPS);
-		assertEquals(rs1,rs2, Ex1.EPS);
+		double x1 = -4, x2 = 0;
+		double rs1 = Ex1.sameValue(po1, po2, x1, x2, Ex1.EPS);
+		double rs2 = Ex1.sameValue(po2, po1, x1, x2, Ex1.EPS);
+		assertEquals(rs1, rs2, Ex1.EPS);
 	}
+
 	@Test
 	/**
 	 * Test the area function - it should be symmetric.
 	 */
 	public void testArea() {
-		double x1=-4, x2=0;
+		double x1 = -4, x2 = 0;
 		double a1 = Ex1.area(po1, po2, x1, x2, 100);
 		double a2 = Ex1.area(po2, po1, x1, x2, 100);
-		assertEquals(a1,a2, Ex1.EPS);
-}
+		assertEquals(a1, a2, Ex1.EPS);
+	}
+
 	@Test
 	/**
 	 * Test the area f1(x)=0, f2(x)=x;
 	 */
 	public void testArea2() {
 		double[] po_a = Ex1.ZERO;
-		double[] po_b = {0,1};
+		double[] po_b = {0, 1};
 		double x1 = -1;
 		double x2 = 2;
-		double a1 = Ex1.area(po_a,po_b, x1, x2, 1);
-		double a2 = Ex1.area(po_a,po_b, x1, x2, 2);
-		double a3 = Ex1.area(po_a,po_b, x1, x2, 3);
-		double a100 = Ex1.area(po_a,po_b, x1, x2, 100);
-		double area =2.5;
-		assertEquals(a1,area, Ex1.EPS);
-		assertEquals(a2,area, Ex1.EPS);
-		assertEquals(a3,area, Ex1.EPS);
-		assertEquals(a100,area, Ex1.EPS);
+		double a1 = Ex1.area(po_a, po_b, x1, x2, 1);
+		double a2 = Ex1.area(po_a, po_b, x1, x2, 2);
+		double a3 = Ex1.area(po_a, po_b, x1, x2, 3);
+		double a100 = Ex1.area(po_a, po_b, x1, x2, 100);
+		double area = 2.5;
+		assertEquals(a1, area, Ex1.EPS);
+		assertEquals(a2, area, Ex1.EPS);
+		assertEquals(a3, area, Ex1.EPS);
+		assertEquals(a100, area, Ex1.EPS);
 	}
+
 	@Test
 	/**
 	 * Test the area function.
@@ -201,14 +219,26 @@ class Ex1Test {
 		double area = 58.5658;
 		assertEquals(a1, area, Ex1.EPS);
 	}
-		@Test
-		/**
-		 * Basic test for poly()
-		 */
-        void testPolyBasic() {
-			double[] p = {2, 0, 3.1, -1.2};
-			String s = Ex1.poly(p);
-			assertEquals("-1.2x^3 +3.1x^2 +2.0", s);
-		}
+
+	@Test
+	/**
+	 * Basic test for poly()
+	 */
+	void testPolyBasic() {
+		double[] p = {2, 0, 3.1, -1.2};
+		String s = Ex1.poly(p);
+		assertEquals("-1.2x^3 +3.1x^2 +2.0", s);
 	}
+
+	@Test
+	void testSameValueBasic() {
+		double[] p1 = {0, 1};
+		double [] p2 = {1};
+		double x = Ex1.sameValue(p1, p2, 0, 2, Ex1.EPS);
+		assertEquals(1.0, x, 0.01);
+		double y1 = Ex1.f(p1,x);
+		double y2 = Ex1.f(p2,x);
+		assertEquals(y1, y2, 0.01);
+	}
+}
 
